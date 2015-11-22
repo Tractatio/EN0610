09ce14c1c411a4f5cd4f1bdd9f71af6af84f6c8d
Tractatio <Theo2k8@hotmail.co.uk>
2015-11-22 15:11:42 +0000

    Setup

---
+++ usercontrols/stylesheets.php
@@ -0,0 +1,25 @@
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800' rel='stylesheet' type='text/css'>
+
+<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+<link rel="stylesheet" href="/css/normalize.css">
+<link rel="stylesheet" href="/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/fontello.css">
+<link rel="stylesheet" href="/css/animate.css">
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/owl.carousel.css">
+<link rel="stylesheet" href="/css/owl.theme.css">
+<link rel="stylesheet" href="/css/owl.transitions.css">
+<link rel="stylesheet" href="/style.css">
+<link rel="stylesheet" href="/responsive.css">
+<script src="/js/vendor/modernizr-2.6.2.min.js"></script>
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/main.js"></script>
